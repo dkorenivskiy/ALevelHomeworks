5e7aac409dba34bdf1d69629f2b8208206ce20a6
Korenivskyi, Danylo <d.korenivskiy@gmail.com>
2021-11-08 23:02:08 +0200

    added hometask # 1

---
+++ hometask1/hometask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hometask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hometask #1!");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter your birthday");
+            Person firstPerson = new Person();
+            firstPerson.ConstructPerson();
+
+            int customYear = 40;
+            int fortyYears = firstPerson.Year + customYear;
+
+            DateTime nowadays = DateTime.Now;
+
+            firstPerson.PrintData();
+            if (nowadays.Year < fortyYears)
+                Console.WriteLine("Years til he(she) got 40 years old: {0}", firstPerson.yearsTilForty(nowadays, fortyYears));
+            else
+                Console.WriteLine("He(She) already got 40 years old");
+        }
+    }
+}
